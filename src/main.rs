d5686da163f9cac4344cd2615c0a4f4c439b3e20
charles37 <charles37@github.com>
2024-03-25 19:44:40 -0400

    health_check added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+
+use actix_web::{web, App, HttpRequest, HttpServer, HttpResponse, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
 }
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+    
+}
+
+
+
+
+
+
+
