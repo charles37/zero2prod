384f1e1a1224419bfd32f76918811aafcc629b50
charles37 <benprevor@gmail.com>
2024-04-21 23:17:43 -0400

    add clippy to flake

--- flake.nix
+++ flake.nix
@@ -2,27 +2,34 @@
   description = "Rust Development Shell";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
-          extensions = [
-            "rust-src"
-            "rustfmt"
-            "rust-analyzer"
-          ];
-        });
-      in
-      {
+        rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
+          toolchain.default.override {
+            extensions = [
+              "rust-src"
+              "rustfmt"
+              "clippy"
+              "rust-analyzer"
+            ];
+          });
+      in {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           preConfigure = ''
             export RUST_LOG=debug
