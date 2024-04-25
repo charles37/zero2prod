7c3972231fd9aa027f1baba1c06709f01381fb26
charles37 <benprevor@gmail.com>
2024-04-25 14:48:14 -0400

    changing things around trying to debug why github workflow isnt working

--- migrations/20240415174249_create_subscriptions_table.sql
+++ migrations/20240415174249_create_subscriptions_table.sql
@@ -1,9 +1,9 @@
 -- Add migration script here
 -- migrations/{timestamp}_create_subscriptions_table.sql
 CREATE TABLE subscriptions(
-  id uuid NOT NULL,
-  PRIMARY KEY (id),
-  email TEXT NOT NULL UNIQUE,
-  name TEXT NOT NULL,
-  subscribed_at timestamptz NOT NULL
+   id uuid NOT NULL,
+   PRIMARY KEY (id),
+   email TEXT NOT NULL UNIQUE,
+   name TEXT NOT NULL,
+   subscribed_at timestamptz NOT NULL
 );
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -6,9 +6,10 @@ use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
+// Ensure that the `tracing` stack is only initialised once using `once_cell`
 static TRACING: Lazy<()> = Lazy::new(|| {
     let default_filter_level = "info".to_string();
-    let subscriber_name: String = "test".into();
+    let subscriber_name = "test".to_string();
     if std::env::var("TEST_LOG").is_ok() {
         let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
         init_subscriber(subscriber);
@@ -24,20 +25,21 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
+    // The first time `initialize` is invoked the code in `TRACING` is executed.
+    // All other invocations will instead skip execution.
     Lazy::force(&TRACING);
 
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
 
-    let mut configuration = get_configuration().expect("failed to get configuration");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
     configuration.database.database_name = Uuid::new_v4().to_string();
-
     let connection_pool = configure_database(&configuration.database).await;
 
-    let server = run(listener, connection_pool.clone()).expect("Faield to bind address");
+    let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
-
     TestApp {
         address,
         db_pool: connection_pool,
@@ -45,19 +47,19 @@ async fn spawn_app() -> TestApp {
 }
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    // Create database
     let mut connection = PgConnection::connect_with(&config.without_db())
         .await
-        .expect("Failed to connect to postgres");
-
+        .expect("Failed to connect to Postgres");
     connection
-        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .execute(&*format!(r#"CREATE DATABASE "{}";"#, config.database_name))
         .await
-        .expect("failed to create database");
+        .expect("Failed to create database.");
 
+    // Migrate database
     let connection_pool = PgPool::connect_with(config.with_db())
         .await
-        .expect("failed to connect to postgres");
-
+        .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./migrations")
         .run(&connection_pool)
         .await
@@ -68,53 +70,54 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
 
 #[tokio::test]
 async fn health_check_works() {
+    // Arrange
     let app = spawn_app().await;
-    println!("{}", app.address);
-
     let client = reqwest::Client::new();
-    println!("{:?}", client);
 
+    // Act
     let response = client
-        .get(&format!("{}/health_check", app.address))
+        // Use the returned application address
+        .get(&format!("{}/health_check", &app.address))
         .send()
         .await
-        .expect("Failed to execute request");
+        .expect("Failed to execute request.");
 
-    println!("{:?}", response);
-
+    // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
 #[tokio::test]
-async fn subscribe_returns_200_for_valid_form_data() {
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
     let app = spawn_app().await;
     let client = reqwest::Client::new();
-
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    // Act
     let response = client
         .post(&format!("{}/subscriptions", &app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
         .await
-        .expect("Failed to execute request");
+        .expect("Failed to execute request.");
 
-    assert_eq!(response.status().as_u16(), 200);
+    // Assert
+    assert_eq!(200, response.status().as_u16());
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
         .fetch_one(&app.db_pool)
         .await
-        .expect("Failed to fetch saved subscriptions");
+        .expect("Failed to fetch saved subscription.");
 
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
     assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
-async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
+async fn subscribe_returns_a_400_when_data_is_missing() {
     // Arrange
-
     let app = spawn_app().await;
     let client = reqwest::Client::new();
     let test_cases = vec![
@@ -122,6 +125,7 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
         ("email=ursula_le_guin%40gmail.com", "missing the name"),
         ("", "missing both name and email"),
     ];
+
     for (invalid_body, error_message) in test_cases {
         // Act
         let response = client
@@ -131,6 +135,7 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
             .send()
             .await
             .expect("Failed to execute request.");
+
         // Assert
         assert_eq!(
             400,
@@ -142,31 +147,34 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
     }
 }
 
-//#[tokio::test]
-//async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
-//    // Arrange
-//    let app = spawn_app().await;
-//    let client = reqwest::Client::new();
-//    let test_cases = vec![
-//        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
-//        ("name=Ursula&email=", "empty email"),
-//        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
-//    ];
-//    for (body, description) in test_cases {
-//        // Act
-//        let response = client
-//            .post(&format!("{}/subscriptions", &app.address))
-//            .header("Content-Type", "application/x-www-form-urlencoded")
-//            .body(body)
-//            .send()
-//            .await
-//            .expect("Failed to execute request.");
-//
-//        assert_eq!(
-//            200,
-//            response.status().as_u16(),
-//            "The API did not return a 200 OK when the payload was {}",
-//            description
-//        );
-//    }
-//}
+#[tokio::test]
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
+    // Arrange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+
+    for (body, description) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not return a 400 Bad Request when the payload was {}.",
+            description
+        );
+    }
+}
+
