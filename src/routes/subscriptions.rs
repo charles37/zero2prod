5fbdd584d909c1ae6d8cf198eea0fdd528c3def4
charles37 <benprevor@gmail.com>
2024-04-25 17:19:15 -0400

    chapter 6 done

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -14,6 +14,22 @@ pub struct FormData {
     pub name: String,
 }
 
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(Self { email, name })
+    }
+}
+
+//pub fn parse_subscriber(form: FormData) -> Result<NewSubscriber, String> {
+//    let name = SubscriberName::parse(form.name)?;
+//    let email = SubscriberEmail::parse(form.email)?;
+//    Ok(NewSubscriber { email, name })
+//}
+
 #[tracing::instrument(
     name = "Adding a new subscriber",
     skip(form, pool),
@@ -23,16 +39,10 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
+    let new_subscriber = match form.0.try_into() {
+        Ok(sub) => sub,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    let email = match SubscriberEmail::parse(form.0.email) {
-        Ok(email) => email,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-
-    let new_subscriber = NewSubscriber { name, email };
 
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
